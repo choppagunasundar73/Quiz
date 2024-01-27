8668ac93b804c3b0accd3eb8fb11efa4825290ef
Choppa Guna Sundar <95371411+choppagunasundar73@users.noreply.github.com>
2024-01-27 18:13:00 +0530

    Add files via upload

---
+++ Quiz/js/questions.js
@@ -0,0 +1,184 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the difference between Valampuri and Idampuri ganesha?",
+    answer: "Trunk",
+    image : "images/image1.png",
+    options: [
+      "Head",
+      "Tusk",
+      "Trunk",
+      "Hands"
+    ]
+  },
+  //   {
+  //   numb: 2,
+  //   question: "Components of pillar?",
+  //   answer: "Sathuram,Kattu,Podigai",
+  //   image : "images/image2.png",
+  //   options: [
+  //     "Sathuram,Kattu,Vattam",
+  //     "Sathuram,Kattu,Podigai",
+  //     "Sathuram,Padam,Parasthram",
+  //     "Padam,Prastharam,kandam"
+  //   ]
+  // },
+  //   {
+  //   numb: 3,
+  //   question: "Cave temples are mainly located on the?",
+  //   answer: "All of these",
+  //   image : "images/image3.png",
+  //   options: [
+  //     "Top of the hillock",
+  //     "Foot of the hillock",
+  //     "Mid of the hillock",
+  //     "All of these"
+  //   ]
+  // },
+  //   {
+  //   numb: 4,
+  //   question: "This Cave Temple comes under which category of style?",
+  //   answer: "Cut in",
+  //   image : "images/image4.png",
+  //   options: [
+  //     "Cut in",
+  //     "Cut out",
+  //     "Structural",
+  //     "Bas Relief"
+  //   ]
+  // },
+  //   {
+  //   numb: 5,
+  //   question: "Name of the sculpture standing both side of the Garbhagraha?",
+  //   answer: "Dhuvarapala",
+  //   image : "images/image5.png",
+  //   options: [
+  //     "Siva",
+  //     "Sankara",
+  //     "Dakshinamurthi",
+  //     "Dhuvarapala"
+  //   ]
+  // },
+
+  //   {
+  //   numb: 6,
+  //   question: "Which one is called as Somaskandha panel?",
+  //   answer: "Siva,Parvathi,Skandha",
+  //   image : "images/image6.png",
+  //   options: [
+  //     "Siva,Parvathi,Ganesha",
+  //     "Parvathi,Siva,Vishnu",
+  //     "Siva,Parvathi,Skandha",
+  //     "Skandha,Vishnu,Siva"
+  //   ]
+  // },
+  // {
+  //   numb: 7,
+  //   question: "Nataraja Sculpture panel is present in which cave temple in Tamil Nadu?",
+  //   answer: "Thiruparankundram",
+  //   image : "images/image7.png",
+  //   options: [
+  //     "Thiruparankundram",
+  //     "Narthamalai",
+  //     "Sittannavasal",
+  //     "Thirumayam"
+  //   ]
+  // },
+  // {
+  //   numb: 8,
+  //   question: "Hayagriva bas relief sculpture present in which cave temple in Tamil Nadu?",
+  //   answer: "Malayadipatti",
+  //   image : "images/image8.png",
+  //   options: [
+  //     "Tantonrimalai",
+  //     "Tiruttangal",
+  //     "Tiruppanjili",
+  //     "Malayadipatti"
+  //   ]
+  // },
+  // {
+  //   numb: 9,
+  //   question: "Which Tamil Nadu cave temple features the Lingothbhava bas-relief sculpture?",
+  //   answer: "Thirumayam",
+  //   image : "images/image9.png",
+  //   options: [
+  //     "Mangudi",
+  //     "Mangulam",
+  //     "Thirumayam",
+  //     "Sivagiri"
+  //   ]
+  // },
+  // {
+  //   numb: 10,
+  //   question: "Which cave contains the inscription referencing Pungundranadu, the birthplace of Kaniyan Pungundarnar?",
+  //   answer: "Maghibalanpatti",
+  //   image : "images/image10.png",
+  //   options: [
+  //     "Maghibalanpatti",
+  //     "Kundrakudi",
+  //     "Tirucchirappalli",
+  //     "Sivagiri"
+  //   ]
+  // },
+  // {
+  //   numb: 11,
+  //   question: "Musical notes inscriptions were present in which cave temple in Tamil Nadu?",
+  //   answer: "Kudumiyanmalai",
+  //   image : "images/image11.png",
+  //   options: [
+  //     "Narthamalai",
+  //     "Kudumiyanmalai",
+  //     "Pilliyarpatti",
+  //     "Palayiliswaram"
+  //   ]
+  // },
+  // {
+  //   numb: 12,
+  //   question: "Parivadini inscriptions were present in which cave temple in Tamil Nadu?",
+  //   answer: "Kudumiyanmalai",
+  //   image : "images/image12.png",
+  //   options: [
+  //     "Narthamalai",
+  //     "Kurattyarai",
+  //     "Pilliyarpatti",
+  //     "Kudumiyanmalai"
+  //   ]
+  // },
+  // {
+  //   numb: 13,
+  //   question: "Standing form of Ganesha were present in which cave temple in Tamil Nadu?",
+  //   answer: "Tiruchchirapalli",
+  //   image : "",
+  //   options: [
+  //     "Tiruchchirapalli",
+  //     "Kottaiyur",
+  //     "Tevarmalai",
+  //     "Pechchiparai"
+  //   ]
+  // },
+  // {
+  //   numb: 14,
+  //   question: "Which Cave painting is compared with Ajantha painting in Tamil Nadu?",
+  //   answer: "Sittannavasal",
+  //   image : "images/image14.png",
+  //   options: [
+  //     "Sittannavasal",
+  //     "Kunnatthur",
+  //     "Kunnakudi",
+  //     "Thiruvellari"
+  //   ]
+  // },
+  // {
+  //   numb: 15,
+  //   question: "Which Cut-Out temple called as Ellora of Tamil Nadu?",
+  //   answer: "Kazhukumalai",
+  //   image : "images/image15.png",
+  //   options: [
+  //     "Narthamalai",
+  //     "Kazhukumalai",
+  //     "Pilliyarpatti",
+  //     "Tiruchendur"
+  //   ]
+  // },
+];
